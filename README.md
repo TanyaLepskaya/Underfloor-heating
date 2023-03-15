6f1c789c902a6f1bb3a926a88a5c72b228b8e9a4
Tanya <123215176+TanyaLepskaya@users.noreply.github.com>
2023-03-15 10:56:33 +0300

    123

---
+++ README.md
@@ -0,0 +1,9 @@
+# 📦 Габитов и партнёры
+Юридические услуги
+
+Webpack 5 
+Start working
+
+npm i
+npm start
+npm run build
---
+++ src/index.js
@@ -0,0 +1,15 @@
+// import 'bootstrap/dist/css/bootstrap.min.css'; //all styles
+// import 'bootstrap'; //all scripts
+
+// const $ = require( "jquery" ); //jQuery
+
+// import 'slick-carousel'
+// import 'slick-carousel/slick/slick.scss'
+// import 'slick-carousel/slick/slick-theme.scss' //SLICK SLIDER
+
+// import 'lightbox2'
+// import 'lightbox2/dist/css/lightbox.min.css' //Scale IMG on click
+
+import 'normalize.css';
+
+import './styles/index.scss';
